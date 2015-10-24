d4d5c274e52932579c66d9361e8597d45b923e5a
Christian Stangier <mail@christian-stangier.com>
2015-10-25 01:39:49 +0200

    add some logging to scripts

--- backup.sh
+++ backup.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/bin/sh
+set -e
 
 # Get timestamp
 : ${BACKUP_SUFFIX:=.$(date +"%Y-%m-%d-%H-%M-%S")}
@@ -8,11 +9,19 @@ readonly tarball=$BACKUP_NAME$BACKUP_SUFFIX.tar.gz
 tar czf $tarball $BACKUP_TAR_OPTION $PATHS_TO_BACKUP
 
 # Create bucket, if it doesn't already exist
-BUCKET_EXIST=$(aws s3 ls | grep $S3_BUCKET_NAME | wc -l)
-if [ $BUCKET_EXIST -eq 0 ]; 
+BUCKET_EXISTS=$(aws s3 ls | grep $S3_BUCKET_NAME | wc -l)
+if [ $BUCKET_EXISTS -eq 0 ];
 then
   aws s3 mb s3://$S3_BUCKET_NAME
 fi
 
 # Upload the backup to S3 with timestamp
-aws s3 cp $tarball s3://$S3_BUCKET_NAME/$tarball
+UPLOAD_SUCCESSFUL=$(aws s3 cp $tarball s3://$S3_BUCKET_NAME/$tarball)
+if [ $UPLOAD_SUCCESSFUL -eq 0 ]
+then
+  echo "Backup of $tarball finished"
+else
+  echo "Backup of $tarball failed"
+fi
+
+rm $tarball
