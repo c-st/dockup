7bbd795165c9e968628a0e8495ce5675c01961c8
Christian Stangier <mail@christian-stangier.com>
2015-10-25 02:42:53 +0200

    improve result checks

--- backup.sh
+++ backup.sh
@@ -10,14 +10,15 @@ tar czf $tarball $BACKUP_TAR_OPTION $PATHS_TO_BACKUP
 
 # Create bucket, if it doesn't already exist
 BUCKET_EXISTS=$(aws s3 ls | grep $S3_BUCKET_NAME | wc -l)
-if [ $BUCKET_EXISTS -eq 0 ];
+if [ "$BUCKET_EXISTS" -eq 0 ];
 then
   aws s3 mb s3://$S3_BUCKET_NAME
 fi
 
 # Upload the backup to S3 with timestamp
-UPLOAD_SUCCESSFUL=$(aws s3 cp $tarball s3://$S3_BUCKET_NAME/$tarball)
-if [ $UPLOAD_SUCCESSFUL -eq 0 ]
+aws s3 cp $tarball s3://$S3_BUCKET_NAME/$tarball
+
+if [ $? -eq 0 ];
 then
   echo "Backup of $tarball finished"
 else
